0905284b1d650172ea3f3b122820acae4e15b831
Encaik <843391622@qq.com>
2019-10-23 16:11:39 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	title: 'Encaik个人主页',
+	description: 'Encaik的个人主页',
+	themeConfig: {
+		sidebar: 'auto',
+		nav: [
+			{ text: '首页', link: '/' },
+			{ text: '博文', link: '/blog/' },
+			{ text: '杂记', link: '/note/' },
+			{ text: 'Github', link: 'https://google.com' },
+		],
+		sidebar: {
+			'/blog/': [
+				'',
+			],
+
+			'/note/': [
+				'',
+			],
+		},
+		sidebarDepth: 2,
+		lastUpdated: '发布日期',
+	}
+}
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+heroText: Encaik前端博客
+features:
+- title: 博文
+  details: 记录项目中遇到的问题及有趣的文章
+- title: 杂记
+  details: 记录学习中的点点滴滴，以及每一次收获
+- title: 项目
+  details: 展示GitHub及Gitee等平台上的仓库项目
+footer: MIT Licensed | Copyright © 2019-Encaik
+---
