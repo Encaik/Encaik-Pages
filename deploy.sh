bf3e3614823e16a0afefe470d547e390520f7f79
Encaik <843391622@qq.com>
2019-10-24 15:26:12 +0800

    修改代码

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f https://github.com/Encaik/Blog.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Encaik/Blog.git master:gh-pages
+git push -f git@github.com:Encaik/blog.git master:gh-pages
 
 cd -
