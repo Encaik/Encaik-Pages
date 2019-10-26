24e662fe666541e2351416f130701ccd2c100695
Encaik <843391622@qq.com>
2019-10-26 23:48:58 +0800

    updata 10.26

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,38 @@ module.exports = {
 	dest: 'docs/.vuepress/blog/',
 	title: 'Encaik-个人博客',
 	description: 'Encaik-个人博客',
+	head: [
+		['link', {
+			rel: 'icon',
+			href: '/img/favicon.ico'
+		}]
+	],
 	themeConfig: {
 		sidebar: 'auto',
-		nav: [
-			{ text: '首页', link: '/' },
-			{ text: '博文', link: '/blog/' },
-			{ text: '杂记', link: '/note/' },
-			{ text: 'Github', link: 'https://github.com/Encaik' },
+		nav: [{
+				text: '首页',
+				link: '/'
+			},
+			{
+				text: '博文',
+				link: '/blog/'
+			},
+			{
+				text: '杂记',
+				link: '/note/'
+			},
+			{
+				text: '学习路径',
+				link: '/road/'
+			},
+			{
+				text: 'Github',
+				link: 'https://github.com/Encaik'
+			},
+			{
+				text: '码云',
+				link: 'https://gitee.com/encaik'
+			}
 		],
 		sidebar: {
 			'/blog/': [
