022f8daf9681b6967856952a33c5fdcb0d691a0f
Encaik <843391622@qq.com>
2020-06-01 10:16:51 +0800

    修改代码

---
+++ docs/.vuepress/theme/components/Page.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="page">
+    <slot name="top" />
+
+    <!-- <img style="margin-top:5rem" src="/img/bg.png" width="100%" height="300px" alt=""> -->
+    <Content class="theme-default-content" />
+    <PageEdit />
+
+    <PageNav v-bind="{ sidebarItems }" />
+
+    <slot name="bottom" />
+  </main>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+import PageNav from '@theme/components/PageNav.vue'
+
+export default {
+  components: { PageEdit, PageNav },
+  props: ['sidebarItems']
+}
+</script>
+
+<style lang="stylus">
+@require '../styles/wrapper.styl'
+
+.page
+  padding-bottom 2rem
+  display block
+
+</style>
