37a1ed276379fb09e2430d47351219f522ba0354
Encaik <843391622@qq.com>
2020-08-27 18:57:26 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env sh
-
-# 确保脚本抛出遇到的错误
 set -e
-
-# 生成静态文件
 npm run docs:build
-
-# 进入生成的文件夹
 cd docs/.vuepress/blog
-
 git init
 git add -A
 git commit -m 'deploy'
 git push -f https://gitee.com/encaik/encaik.git master
+cd ../
+git rm -r -f ./blog
